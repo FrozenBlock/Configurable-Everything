54489178375a7a632946e7ff6645c47330a6418a
Treetrain1 <treetrain1@gmail.com>
2023-08-13 19:42:44 -0500

    add main config gui

--- src/main/java/net/frozenblock/configurableeverything/config/gui/EntityConfigGui.java
+++ src/main/java/net/frozenblock/configurableeverything/config/gui/EntityConfigGui.java
@@ -5,49 +5,49 @@ import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.frozenblock.configurableeverything.config.EntityConfig;
-import net.frozenblock.configurableeverything.config.gui.main.ConfigurableEverythingConfigGui;
 import net.frozenblock.lib.config.clothconfig.FrozenClothConfig;
+
+import static net.frozenblock.configurableeverything.config.gui.main.ConfigurableEverythingConfigGui.*;
 
 @Environment(EnvType.CLIENT)
 public final class EntityConfigGui {
 
 	public static void setupEntries(ConfigCategory category, ConfigEntryBuilder entryBuilder) {
 		var config = EntityConfig.get();
+		category.setBackground(id("textures/config/entity.png"));
 
 		var player = config.player;
-		var digSpeedAmplifier = entryBuilder.startIntSlider(ConfigurableEverythingConfigGui.text("dig_speed_amplifier"), player.digSpeedAmplifier, 1, 5000)
+		var digSpeedAmplifier = entryBuilder.startIntSlider(text("dig_speed_amplifier"), player.digSpeedAmplifier, 1, 5000)
 			.setDefaultValue(100)
 			.setSaveConsumer(newValue -> player.digSpeedAmplifier = newValue)
-			.setTooltip(ConfigurableEverythingConfigGui.tooltip("dig_speed_amplifier"))
+			.setTooltip(tooltip("dig_speed_amplifier"))
 			.build();
 
-		var playerCategory = FrozenClothConfig.createSubCategory(entryBuilder, category, ConfigurableEverythingConfigGui.text("player"),
+		var playerCategory = FrozenClothConfig.createSubCategory(entryBuilder, category, text("player"),
 			false,
-			ConfigurableEverythingConfigGui.tooltip("player"),
+			tooltip("player"),
 			digSpeedAmplifier
 		);
 
 		var zombie = config.zombie;
-		var babyZombieSprint = entryBuilder.startBooleanToggle(ConfigurableEverythingConfigGui.text("baby_zombie_sprint_particles"), zombie.babyZombieSprintParticles)
+		var babyZombieSprint = entryBuilder.startBooleanToggle(text("baby_zombie_sprint_particles"), zombie.babyZombieSprintParticles)
 			.setDefaultValue(true)
 			.setSaveConsumer(newValue -> zombie.babyZombieSprintParticles = newValue)
-			.setTooltip(ConfigurableEverythingConfigGui.tooltip("baby_zombie_sprint_particles"))
-			.setYesNoTextSupplier(bool -> ConfigurableEverythingConfigGui.text(String.valueOf(bool)))
+			.setTooltip(tooltip("baby_zombie_sprint_particles"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
 			.build();
-		var allZombiesBreakDoors = entryBuilder.startBooleanToggle(ConfigurableEverythingConfigGui.text("all_zombies_break_doors"), zombie.allZombiesBreakDoors)
+		var allZombiesBreakDoors = entryBuilder.startBooleanToggle(text("all_zombies_break_doors"), zombie.allZombiesBreakDoors)
 			.setDefaultValue(true)
 			.setSaveConsumer(newValue -> zombie.allZombiesBreakDoors = newValue)
-			.setTooltip(ConfigurableEverythingConfigGui.tooltip("all_zombies_break_doors"))
-			.setYesNoTextSupplier(bool -> ConfigurableEverythingConfigGui.text(String.valueOf(bool)))
+			.setTooltip(tooltip("all_zombies_break_doors"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
 			.build();
 
-		var zombieCategory = FrozenClothConfig.createSubCategory(entryBuilder, category, ConfigurableEverythingConfigGui.text("zombie"),
+		var zombieCategory = FrozenClothConfig.createSubCategory(entryBuilder, category, text("zombie"),
 			false,
-			ConfigurableEverythingConfigGui.tooltip("zombie"),
+			tooltip("zombie"),
 			allZombiesBreakDoors, babyZombieSprint
 		);
-
-		category.setBackground(ConfigurableEverythingConfigGui.id("textures/config/entity.png"));
 	}
 
 }
---
+++ src/main/java/net/frozenblock/configurableeverything/config/gui/MainConfigGui.java
@@ -0,0 +1,111 @@
+package net.frozenblock.configurableeverything.config.gui;
+
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.frozenblock.configurableeverything.config.MainConfig;
+import net.frozenblock.lib.config.clothconfig.FrozenClothConfig;
+import net.minecraft.network.chat.Component;
+
+import static net.frozenblock.configurableeverything.config.gui.main.ConfigurableEverythingConfigGui.*;
+
+@Environment(EnvType.CLIENT)
+public class MainConfigGui {
+
+	public static void setupEntries(ConfigCategory category, ConfigEntryBuilder entryBuilder) {
+		var config = MainConfig.get();
+		category.setBackground(id("textures/config/main.png"));
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("biome"), config.biome)
+				.setDefaultValue(false)
+				.setSaveConsumer(newValue -> config.biome = newValue)
+				.setTooltip(tooltip("biome"))
+				.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("biome_placement"), config.biome_placement)
+			.setDefaultValue(false)
+			.setSaveConsumer(newValue -> config.biome_placement = newValue)
+			.setTooltip(tooltip("biome_placement"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("datafixer"), config.datafixer)
+			.setDefaultValue(false)
+			.setSaveConsumer(newValue -> config.datafixer = newValue)
+			.setTooltip(tooltip("datafixer"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("entity"), config.entity)
+			.setDefaultValue(false)
+			.setSaveConsumer(newValue -> config.entity = newValue)
+			.setTooltip(tooltip("entity"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("fluid"), config.fluid)
+			.setDefaultValue(false)
+			.setSaveConsumer(newValue -> config.fluid = newValue)
+			.setTooltip(tooltip("fluid"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("screen_shake"), config.screen_shake)
+			.setDefaultValue(false)
+			.setSaveConsumer(newValue -> config.screen_shake = newValue)
+			.setTooltip(tooltip("screen_shake"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("splash_text"), config.splash_text)
+			.setDefaultValue(false)
+			.setSaveConsumer(newValue -> config.splash_text = newValue)
+			.setTooltip(tooltip("splash_text"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("surface_rule"), config.surface_rule)
+			.setDefaultValue(false)
+			.setSaveConsumer(newValue -> config.surface_rule = newValue)
+			.setTooltip(tooltip("surface_rule"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		category.addEntry(entryBuilder.startBooleanToggle(text("world"), config.world)
+			.setDefaultValue(false)
+			.setSaveConsumer(newValue -> config.world = newValue)
+			.setTooltip(tooltip("world"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build()
+		);
+
+		var applyDatapacksFolder = entryBuilder.startBooleanToggle(text("applyDatapacksFolder"), config.datapack.applyDatapacksFolder)
+			.setDefaultValue(true)
+			.setSaveConsumer(newValue -> config.datapack.applyDatapacksFolder = newValue)
+			.setTooltip(tooltip("applyDatapacksFolder"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build();
+
+		var datapackBiomePlacement = entryBuilder.startBooleanToggle(text("datapack_biome_placement"), config.datapack.biome_placement)
+			.setDefaultValue(true)
+			.setSaveConsumer(newValue -> config.datapack.biome_placement = newValue)
+			.setTooltip(tooltip("datapack_biome_placement"))
+			.setYesNoTextSupplier(bool -> text(String.valueOf(bool)))
+			.build();
+
+		FrozenClothConfig.createSubCategory(
+			entryBuilder, category, text("datapack"), false, tooltip("datapack"),
+			applyDatapacksFolder, datapackBiomePlacement
+		);
+	}
+}
