4dbc4e17df72a47657675cb49b98da3e982e6813
Treetrain1 <treetrain1@gmail.com>
2023-08-18 22:57:49 -0500

    use frozenlib config overrides

--- src/main/java/net/frozenblock/configurableeverything/world/util/WorldConfigUtil.kt
+++ src/main/java/net/frozenblock/configurableeverything/world/util/WorldConfigUtil.kt
@@ -2,7 +2,7 @@ package net.frozenblock.configurableeverything.world.util
 
 import net.frozenblock.configurableeverything.config.MainConfig
 import net.frozenblock.configurableeverything.config.WorldConfig
-import net.frozenblock.lib.config.frozenlib_config.FrozenLibConfig
+import net.frozenblock.lib.config.frozenlib_config.FrozenLibConfigOverrides
 
 object WorldConfigUtil {
 
@@ -10,6 +10,6 @@ object WorldConfigUtil {
     fun init() {
         val config = WorldConfig.get()
         if (MainConfig.get().world && config.disableExperimentalWarning)
-            FrozenLibConfig.get().removeExperimentalWarning = true
+            FrozenLibConfigOverrides.removeExperimentalWarning = true
     }
 }
