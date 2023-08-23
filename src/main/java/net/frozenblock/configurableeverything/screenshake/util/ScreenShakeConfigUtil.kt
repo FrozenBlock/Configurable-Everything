76c62568f3c68c22d25ef82d8adb6cdf9e1e2d32
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-08-23 12:05:31 -0500

    crazy?

---
+++ src/main/java/net/frozenblock/configurableeverything/screenshake/util/ScreenShakeConfigUtil.kt
@@ -0,0 +1,56 @@
+package net.frozenblock.configurableeverything.screenshake.util
+
+import java.util.List
+import net.frozenblock.configurableeverything.config.MainConfig
+import net.frozenblock.configurableeverything.config.ScreenShakeConfig
+import net.frozenblock.lib.screenshake.api.ScreenShakeManager
+import net.frozenblock.lib.screenshake.api.client.ScreenShaker
+import net.minecraft.client.multiplayer.ClientLevel
+import net.minecraft.sounds.SoundEvent
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.level.Level
+import net.minecraft.world.phys.AABB
+import net.minecraft.world.phys.Vec3
+
+object ScreenShakeConfigUtil {
+
+    @JvmStatic
+    fun createScreenShake(level: Level?, x: Double?, y: Double?, z: Double?, sound: SoundEvent?) {
+        var config = ScreenShakeConfig.get()
+        if (MainConfig.get().screen_shake == true) {
+            if (level == null || x == null || y == null || z == null || sound == null) return
+            val offset: Double = 0.001
+            val entities: List<Entity> = level.getEntities(null, AABB(x - offset, y - offset, z - offset, x + offset, y + offset, z + offset))
+            if (config.soundScreenShakes?.value != null) {
+                var shakes = config.soundScreenShakes.value
+                for (SoundScreenShake shake : shakes) {
+                    if (shake.sound().equals(sound.getLocation())) {
+                        if (entities.isEmpty()) { // apply to position if no entity is found
+                            if (level.isClientSide) {
+                                var client = level as ClientLevel
+                                ScreenShaker.SCREEN_SHAKES.add(
+                                        ScreenShaker.ClientScreenShake(
+                                                client, shake.intensity(), shake.duration(), shake.falloffStart(), Vec3(x, y, z), shake.maxDistance(), 0
+                                        )
+                                )
+                            } else {
+                                ScreenShakeManager.addScreenShake(level, shake.intensity(), shake.duration(), shake.falloffStart(), x, y, z, shake.maxDistance())
+                            }
+                        } else { // find an entity to apply the screen shake to
+                            var entity = entities.stream().findFirst().get();
+                            if (level.isClientSide) {
+                                ScreenShaker.SCREEN_SHAKES.add(
+                                        ScreenShaker.ClientEntityScreenShake(
+                                                entity, shake.intensity(), shake.duration(), shake.falloffStart(), shake.maxDistance(), 0
+                                        )
+                                )
+                            } else {
+                                ScreenShakeManager.addEntityScreenShake(entity, shake.intensity(), shake.duration(), shake.falloffStart(), shake.maxDistance())
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
