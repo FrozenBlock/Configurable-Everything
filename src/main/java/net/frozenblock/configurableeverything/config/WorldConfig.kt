1cb4df8b8f7a5b04f3a1d70b32fea67b7312bf85
Treetrain1 <treetrain1@gmail.com>
2023-08-25 21:51:28 -0500

    real

---
+++ src/main/java/net/frozenblock/configurableeverything/config/WorldConfig.kt
@@ -0,0 +1,52 @@
+package net.frozenblock.configurableeverything.config
+
+import blue.endless.jankson.Comment
+import net.frozenblock.configurableeverything.util.CONFIG_JSONTYPE
+import net.frozenblock.configurableeverything.util.MOD_ID
+import net.frozenblock.configurableeverything.util.makeConfigPath
+import net.frozenblock.lib.config.api.instance.Config
+import net.frozenblock.lib.config.api.instance.json.JsonConfig
+import net.frozenblock.lib.config.api.registry.ConfigRegistry
+
+class WorldConfig {
+    companion object {
+        val INSTANCE: Config<WorldConfig> = ConfigRegistry.register(
+            JsonConfig(
+                MOD_ID,
+                WorldConfig::class.java,
+                makeConfigPath("world"),
+                CONFIG_JSONTYPE
+            )
+        )
+
+        @JvmStatic
+        fun getSunSize(): Float = get().sunSize / 10F
+
+        @JvmStatic
+        fun getMoonSize(): Float = get().moonSize / 10F
+
+        @JvmStatic
+        fun get(): WorldConfig = INSTANCE.config()
+
+        @JvmStatic
+        fun getConfigInstance(): Config<WorldConfig> = INSTANCE
+    }
+
+    @JvmField
+	@Comment("Does not modify tick rate. Only modifies daytime speed.")
+    var dayTimeSpeedAmplifier: Long = 1
+    @JvmField
+	var fixSunMoonRotating = false
+
+    @JvmField
+	@Comment("Incompatible with mod Bedrockify.")
+    var sunSize = 300
+    @JvmField
+	var moonSize = 200
+    @JvmField
+	var flameBowsLightFire = false
+
+    @JvmField
+	@Comment("Disables the experimental warning screen when creating or loading worlds.")
+    var disableExperimentalWarning = false
+}
