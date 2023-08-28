332151b5d2eef0e7d25d3920422e43ccee5408f9
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-08-28 11:31:40 -0500

    attempt to make typed entry on cloth config

--- src/main/java/net/frozenblock/configurableeverything/config/GameConfig.kt
+++ src/main/java/net/frozenblock/configurableeverything/config/GameConfig.kt
@@ -4,13 +4,23 @@ import blue.endless.jankson.Comment
 import net.frozenblock.configurableeverything.util.CONFIG_JSONTYPE
 import net.frozenblock.configurableeverything.util.MOD_ID
 import net.frozenblock.configurableeverything.util.makeConfigPath
+import net.frozenblock.lib.config.api.entry.TypedEntryType
+import net.frozenblock.lib.config.api.entry.TypedEntry
 import net.frozenblock.lib.config.api.instance.Config
 import net.frozenblock.lib.config.api.instance.json.JsonConfig
 import net.frozenblock.lib.config.api.instance.json.JsonType
 import net.frozenblock.lib.config.api.registry.ConfigRegistry
+import net.minecraft.world.phys.Vec3
 
 class GameConfig {
     companion object {
+        private val TEST: TypedEntryType<List<Vec3>> = ConfigRegistry.register(
+            TypedEntryType(
+                MOD_ID,
+                Vec3.CODEC.listOf()
+            )
+        )
+
         private val INSTANCE: Config<GameConfig> = ConfigRegistry.register(
             JsonConfig(
                 MOD_ID,
@@ -40,4 +50,15 @@ class GameConfig {
 		"""
     )
     var versionSeries: String? = ""
+
+    @JvmField
+    @Comment("Im just testing Cloth Config typed entries")
+    var testing: TypedEntry<List<Vec3>>? = TypedEntry(
+        TEST,
+        listOf(
+            Vec3(1, 1, 1),
+            Vec3(2, 2, 2),
+            Vec3(69, 420, 5)
+        )
+    )
 }
--- src/main/java/net/frozenblock/configurableeverything/config/gui/GameConfigGui.kt
+++ src/main/java/net/frozenblock/configurableeverything/config/gui/GameConfigGui.kt
@@ -1,7 +1,7 @@
 package net.frozenblock.configurableeverything.config.gui
 
-import me.shedaniel.clothconfig2.api.ConfigCategory
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder
+import me.shedaniel.clothconfig2.api.*
+import me.shedaniel.clothconfig2.gui.entries.*
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.frozenblock.configurableeverything.config.GameConfig
@@ -9,6 +9,7 @@ import net.frozenblock.configurableeverything.util.id
 import net.frozenblock.configurableeverything.util.text
 import net.frozenblock.configurableeverything.util.tooltip
 import net.frozenblock.lib.config.clothconfig.FrozenClothConfig
+import net.minecraft.network.chat.Component
 
 @Environment(EnvType.CLIENT)
 object GameConfigGui {
@@ -29,5 +30,39 @@ object GameConfigGui {
             .setTooltip(tooltip("versionSeries"))
             .build()
         )
+
+        category.addEntry(NestedListListEntry(
+            Component.literal("Test"),
+            config.testing,
+            false,
+            Optional::empty,
+            { newValue -> config.testing.value = newValue},
+            { listOf(Vec3(1, 1, 1), Vec3(2, 2, 2), Vec3(69, 420, 5))},
+            entryBuilder.getResetButtonKey(),
+            true,
+            true,
+            { (elem, nestedListListEntry) ->
+                if (elem == null) {
+                    val newDefault = Vec3(1.0, 1.0, 1.0)
+                    return MultiElementListEntry(Component.literal("Vec3"), newDefault,
+                        listOf(
+                            entryBuilder.startDoubleField(Component.literal("x"), newDefault.x).setDefaultValue(1.0).build(),
+                            entryBuilder.startDoubleField(Component.literal("y"), newDefault.y).setDefaultValue(1.0).build()
+                            entryBuilder.startDoubleField(Component.literal("z"), newDefault.z).setDefaultValue(1.0).build()
+                        ),
+                        true
+                    )
+                } else {
+                    return MultiElementListEntry(Component.literal("Vec3"), elem,
+                        listOf(
+                            entryBuilder.startDoubleField(Component.literal("x"), elem.x).setDefaultValue(1.0).build(),
+                            entryBuilder.startDoubleField(Component.literal("y"), elem.y).setDefaultValue(1.0).build(),
+                            entryBuilder.startDoubleField(Component.literal("z"), elem.z).setDefaultValue(1.0).build()
+                        ),
+                        true
+                    )
+                }
+            }
+        ))
     }
 }
