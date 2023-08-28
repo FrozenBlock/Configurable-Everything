631576c33ff339bfd53da1c2cf3a30e207690906
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-08-28 14:02:29 -0500

    i dont know

--- src/main/java/net/frozenblock/configurableeverything/config/gui/GameConfigGui.kt
+++ src/main/java/net/frozenblock/configurableeverything/config/gui/GameConfigGui.kt
@@ -41,9 +41,7 @@ object GameConfigGui {
             false,
             Optional::empty,
             { newValue ->
-                var entry = defaultConfig.testing!!
-                entry = TypedEntry(entry.type(), newValue)
-                config.testing = entry
+                config.testing = TypedEntry(defaultConfig.testing.type, newValue)
             },
             { listOf(Vec3(1.0, 1.0, 1.0), Vec3(2.0, 2.0, 2.0), Vec3(69.0, 420.0, 5.0))},
             entryBuilder.getResetButtonKey(),
