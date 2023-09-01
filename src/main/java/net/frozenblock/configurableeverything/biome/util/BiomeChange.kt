82b4708e512a298e02745e08f77dcfd0a0d0f2d1
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-09-01 07:59:07 -0500

    kt biome change

---
+++ src/main/java/net/frozenblock/configurableeverything/biome/util/BiomeChange.kt
@@ -0,0 +1,23 @@
+package net.frozenblock.configurableeverything.biome.util
+
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+
+data class BiomeChange(
+	@JvmField var addedFeatures: List<BiomePlacedFeatureList?>?,
+	@JvmField var removedFeatures: List<BiomePlacedFeatureList?>?,
+	@JvmField var replacedFeatures: List<BiomePlacedFeatureReplacementList?>?,
+	@JvmField var musicReplacements: List<BiomeMusic?>?
+) {
+	companion object {
+		@JvmField
+		val CODEC: Codec<BiomeChange> = RecordCodecBuilder.create { instance ->
+			instance.group(
+				BiomePlacedFeatureList.CODEC.listOf().fieldOf("addedFeatures").forGetter(BiomeChange::addedFeatures),
+				BiomePlacedFeatureList.CODEC.listOf().fieldOf("removedFeatures").forGetter(BiomeChange::removedFeatures),
+				BiomePlacedFeatureReplacementList.CODEC.listOf().fieldOf("replacedFeatures").forGetter(BiomeChange::replacedFeatures),
+				BiomeMusic.CODEC.listOf().fieldOf("musicReplacements").forGetter(BiomeChange::musicReplacements)
+			).apply(instance, ::BiomeChange)
+		}
+	}
+}
