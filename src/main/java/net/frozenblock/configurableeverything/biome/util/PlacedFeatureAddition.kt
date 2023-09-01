d660aae72b04d4dd80ae54d5b8f3bc902dba794f
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-09-01 13:52:48 -0500

    dynamic addition codec

--- src/main/java/net/frozenblock/configurableeverything/biome/util/PlacedFeatureAddition.kt
+++ src/main/java/net/frozenblock/configurableeverything/biome/util/PlacedFeatureAddition.kt
@@ -4,4 +4,12 @@ import net.frozenblock.configurableeverything.util.DynamicRegistryAddition
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.level.levelgen.placement.PlacedFeature
 
-data class PlacedFeatureAddition(key: ResourceKey<PlacedFeature>, value: PlacedFeature) : DynamicRegistryAddition<PlacedFeature>(Registries.PLACED_FEATURE, key, value)
+data class PlacedFeatureAddition(key: ResourceKey<PlacedFeature>, value: PlacedFeature) : DynamicRegistryAddition<PlacedFeature>(Registries.PLACED_FEATURE, key, value) {
+    companion object {
+        @JvmField
+        val CODEC: Codec<PlacedFeatureAddition> = DynamicRegistryAddition.codec(
+            Registries.PLACED_FEATURE,
+            PlacedFeature.CODEC
+        )
+    }
+}
--- src/main/java/net/frozenblock/configurableeverything/util/DynamicRegistryAddition.kt
+++ src/main/java/net/frozenblock/configurableeverything/util/DynamicRegistryAddition.kt
@@ -5,6 +5,19 @@ import net.minecraft.core.Registry
 import net.minecraft.resources.ResourceKey
 
 data class DynamicRegistryAddition<T>(registry: ResourceKey<out Registry<T>>, key: ResourceKey<T>, value: T) {
+    companion object {
+        @JvmStatic
+        fun <T> codec(registry: ResourceKey<out Registry<T>>, valueCodec: Codec<T>): Codec<DynamicRegistryAddition<T>> {
+            return RecordCodecBuilder.create { instance ->
+                instance.group(
+                    ResourceKey.codec(Registries.REGISTRY).fieldOf("registry").forGetter(DynamicRegistryAddition::registry),
+                    ResourceKey.codec(registry).fieldOf("key").forGetter(DynamicRegistryAddition::key),
+                    valueCodec.fieldOf("value").forGetter(DynamicRegistryAddition::value)
+                )
+            }
+        }
+    }
+
     fun register() {
         TODO()
     }
