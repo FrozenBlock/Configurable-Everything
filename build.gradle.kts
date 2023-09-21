88aa8bc65938a3faa2fdc850c28a1e379a52d1cc
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-09-21 12:10:44 -0500

    1.20.2 is out :exploding_head:

--- build.gradle.kts
+++ build.gradle.kts
@@ -275,7 +275,7 @@ tasks {
         val properties = HashMap<String, Any>()
         properties["mod_id"] = mod_id
         properties["version"] = version
-        properties["minecraft_version"] = "~1.20.2-"
+        properties["minecraft_version"] = minecraft_version
         properties["fabric_kotlin_version"] = fabric_kotlin_version
 
         properties.forEach { (a, b) -> inputs.property(a, b) }
