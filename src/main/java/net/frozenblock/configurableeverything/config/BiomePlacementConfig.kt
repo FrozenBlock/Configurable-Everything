456331ba6761a899b5be031b406be40a4b5e8d2c
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-09-22 13:06:57 -0500

    convert biome placement config to data class

--- src/main/java/net/frozenblock/configurableeverything/config/BiomePlacementConfig.kt
+++ src/main/java/net/frozenblock/configurableeverything/config/BiomePlacementConfig.kt
@@ -3,7 +3,7 @@ package net.frozenblock.configurableeverything.config
 import com.mojang.datafixers.util.Either
 
 import net.frozenblock.configurableeverything.biome_placement.util.*
-import net.frozenblock.configurableeverything.datagen.ConfigurableEverythingDataGenerator
+import net.frozenblock.configurableeverything.datagen.ConfigurableEverythingDataGenerator.*
 import net.frozenblock.configurableeverything.util.CONFIG_JSONTYPE
 import net.frozenblock.configurableeverything.util.MOD_ID
 import net.frozenblock.configurableeverything.util.makeConfigPath
@@ -20,10 +20,90 @@ import net.frozenblock.lib.worldgen.biome.api.parameters.Erosion
 import net.frozenblock.lib.worldgen.biome.api.parameters.Humidity
 import net.frozenblock.lib.worldgen.biome.api.parameters.Temperature
 import net.frozenblock.lib.worldgen.biome.api.parameters.Weirdness
-import net.minecraft.world.level.biome.Climate
+import net.minecraft.world.level.biome.Climate.*
+import net.minecraft.net.minecraft.world.level.biome.Climate.Parameter.*
 import net.minecraft.world.level.dimension.BuiltinDimensionTypes
 
-public class BiomePlacementConfig {
+data class BiomePlacementConfig(
+	@JvmField
+	@Comment(
+"""
+Add any biome to worldgen, with the parameters set.
+Adds biomes after biomes are removed via "removedBiomes",
+so replacing a biome's parameters is possible.
+Supports: Vanilla biomes, datapack biomes, modded biomes
+"""
+	)
+	var addedBiomes: TypedEntry<List<DimensionBiomeList?>>? = TypedEntry(
+		BIOME_PARAMETER_LIST,
+		listOf(
+			DimensionBiomeList(
+				BuiltinDimensionTypes.OVERWORLD,
+				listOf(
+					BiomeParameters(
+						BLANK_BIOME.location(),
+						parameters(
+							Temperature.FULL_RANGE,
+							Humidity.FULL_RANGE,
+							Continentalness.MUSHROOM_FIELDS,
+							Erosion.FULL_RANGE,
+							span(Depth.SURFACE, Depth.FLOOR),
+							Weirdness.FULL_RANGE,
+							0F
+						).mutable()
+					)
+				)
+			),
+			DimensionBiomeList(
+				BuiltinDimensionTypes.NETHER,
+				listOf(
+					BiomeParameters(
+						BLANK_BIOME.location(),
+						parameters(
+							Temperature.FULL_RANGE,
+							Humidity.FULL_RANGE,
+							Continentalness.MUSHROOM_FIELDS,
+							Erosion.FULL_RANGE
+							span(Depth.SURFACE, Depth.FLOOR),
+							Weirdness.FULL_RANGE,
+							0F
+						).mutable()
+					)
+				)
+			)
+		)
+	),
+
+	@JvmField
+	@Comment(
+"""
+Remove any biome from worldgen
+Removes biomes before biomes are added via "addedBiomes",
+so replacing a biome's parameters is possible.
+Supports: Vanilla biomes, datapack biomes, Vanilla biome tags, datapack biome tags
+Does not support biomes added via TerraBlender
+"""
+	)
+	var removedBiomes: TypedEntry<List<DimensionBiomeKeyList?>>? = TypedEntry(
+		BIOME_KEY_LIST,
+		listOf(
+			DimensionBiomeKeyList(
+				BuiltinDimensionTypes.OVERWORLD,
+				listOf(
+					Either.left(BLANK_BIOME),
+					Either.right(BLANK_TAG)
+				)
+			),
+			DimensionBiomeKeyList(
+				BuiltinDimensionTypes.NETHER,
+				listOf(
+					Either.left(BLANK_BIOME),
+					Either.right(BLANK_TAG)
+				)
+			)
+		)
+	)
+) {
 	companion object {
 		private val BIOME_KEY_LIST: TypedEntryType<List<DimensionBiomeKeyList?>> = ConfigRegistry.register(
 			TypedEntryType(
@@ -52,80 +132,4 @@ public class BiomePlacementConfig {
 		@JvmStatic
 		fun get() = INSTANCE.config()
 	}
-
-	@Comment(
-		"""
-		Add any biome to worldgen, with the parameters set.
-		Adds biomes after biomes are removed via "removedBiomes",
-		so replacing a biome's parameters is possible.
-		Supports: Vanilla biomes, datapack biomes, modded biomes
-		"""
-	)
-	var addedBiomes: TypedEntry<List<DimensionBiomeList?>>? = TypedEntry(
-		BIOME_PARAMETER_LIST,
-		listOf(
-			DimensionBiomeList(
-				BuiltinDimensionTypes.OVERWORLD,
-				listOf(
-					BiomeParameters(
-						ConfigurableEverythingDataGenerator.BLANK_BIOME.location(),
-						Climate.parameters(
-							Temperature.FULL_RANGE,
-							Humidity.FULL_RANGE,
-							Continentalness.MUSHROOM_FIELDS,
-							Erosion.FULL_RANGE,
-							Climate.Parameter.span(Depth.SURFACE, Depth.FLOOR),
-							Weirdness.FULL_RANGE,
-							0F
-						).mutable()
-					)
-				)
-			),
-			DimensionBiomeList(
-				BuiltinDimensionTypes.NETHER,
-				listOf(
-					BiomeParameters(
-						ConfigurableEverythingDataGenerator.BLANK_BIOME.location(),
-						Climate.parameters(
-							Climate.Parameter.span(-1F, 1F),
-							Climate.Parameter.span(-1F, 1F),
-							Continentalness.MUSHROOM_FIELDS,
-							Climate.Parameter.span(-1F, 1F),
-							Climate.Parameter.span(-1F, 1F),
-							Climate.Parameter.span(-1F, 1F),
-							0F
-						).mutable()
-					)
-				)
-			)
-		)
-	)
-
-	@Comment(
-		"""
-		Remove any biome from worldgen
-		Removes biomes before biomes are added via "addedBiomes",
-		so replacing a biome's parameters is possible.
-		Supports: Vanilla biomes, datapack biomes, Vanilla biome tags, datapack biome tags
-		Does not support biomes added via TerraBlender
-		"""
-	)
-	var removedBiomes: TypedEntry<List<DimensionBiomeKeyList?>>? = TypedEntry(
-		BIOME_KEY_LIST,
-		listOf(
-			DimensionBiomeKeyList(
-				BuiltinDimensionTypes.OVERWORLD,
-				listOf(
-					Either.left(ConfigurableEverythingDataGenerator.BLANK_BIOME),
-					Either.right(ConfigurableEverythingDataGenerator.BLANK_TAG)
-				)
-			),
-			DimensionBiomeKeyList(
-				BuiltinDimensionTypes.NETHER,
-				listOf(
-					Either.left(ConfigurableEverythingDataGenerator.BLANK_BIOME)
-				)
-			)
-		)
-	)
 }
