3b392970494ec2dc715584d242c050d238e0f620
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-10-04 13:55:05 -0500

    fix??

--- src/main/java/net/frozenblock/configurableeverything/scripting/util/ScriptingInteractions.kt
+++ src/main/java/net/frozenblock/configurableeverything/scripting/util/ScriptingInteractions.kt
@@ -6,20 +6,20 @@ import net.minecraft.core.registries.Registries as VanillaRegistries
 import net.minecraft.resources.ResourceKey as VanillaResourceKey
 import net.minecraft.resources.ResourceLocation as VanillaResourceLocation
 
-protected object Registries {
+object Registries {
     val BLOCK = VanillaRegistries.BLOCK
     val ENTITY_TYPE = VanillaRegistries.ENTITY_TYPE
     val ITEM = VanillaRegistries.ITEM
 }
 
-protected object BuiltInRegistries {
+object BuiltInRegistries {
     val BLOCK = Registry(VanillaBuiltInRegistries.BLOCK)
     val ENTITY_TYPE = Registry(VanillaBuiltInRegistries.ENTITY_TYPE)
     val ITEM = Registry(VanillaBuiltInRegistries.ITEM)
 }
 
-protected data class Registry<T>(
-    val registry = VanillaRegistry<T>
+data class Registry<T>(
+    val registry: VanillaRegistry<T>
 ) : FakeObject<VanillaRegistry<T>> {
 
     fun register(id: ResourceLocation, value: FakeObject<T>): FakeObject<T> {
@@ -37,18 +37,18 @@ protected data class Registry<T>(
     }
 }
 
-protected data class ResourceKey<T>(val registry: ResourceKey<Registry<T>>?, id: ResourceLocation) : FakeObject<VanillaResourceKey<T>> {
+data class ResourceKey<T>(val registry: ResourceKey<Registry<T>>?, id: ResourceLocation) : FakeObject<VanillaResourceKey<T>> {
     override fun value(): VanillaResourceKey<T> {
         return VanillaResourceKey.create(registry?.value(), id.value())
     }
 }
 
-protected data class ResourceLocation(val namespace: String = "minecraft", val path: String) : FakeObject<VanillaResourceLocation> {
+data class ResourceLocation(val namespace: String = "minecraft", val path: String) : FakeObject<VanillaResourceLocation> {
     override fun value(): VanillaResourceLocation {
         return VanillaResourceLocation(namespace, path)
     }
 }
 
-protected abstract class FakeObject<T> {
-    abstract fun value(): T
+fun interface FakeObject<T> {
+    fun value(): T
 }
