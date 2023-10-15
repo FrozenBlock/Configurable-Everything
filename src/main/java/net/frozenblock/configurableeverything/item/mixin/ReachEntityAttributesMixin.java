3d6803570fb105e835504572e42ff711e0774a60
Treetrain1 <treetrain1@gmail.com>
2023-10-15 16:37:01 -0500

    early priority

--- src/main/java/net/frozenblock/configurableeverything/item/mixin/ReachEntityAttributesMixin.java
+++ src/main/java/net/frozenblock/configurableeverything/item/mixin/ReachEntityAttributesMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ReachEntityAttributes.class)
+@Mixin(value = ReachEntityAttributes.class, priority = 500)
 public class ReachEntityAttributesMixin {
 
 	@Inject(method = "getReachDistance", at = @At("HEAD"), cancellable = true)
