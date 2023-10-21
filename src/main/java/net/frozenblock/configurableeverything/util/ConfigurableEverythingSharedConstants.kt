af0a1709d9b43710ffcc28d4f990a38e0b57d249
Treetrain1 <treetrain1@gmail.com>
2023-10-21 15:11:17 -0500

    client only scripts + improve script logging

--- src/main/java/net/frozenblock/configurableeverything/util/ConfigurableEverythingSharedConstants.kt
+++ src/main/java/net/frozenblock/configurableeverything/util/ConfigurableEverythingSharedConstants.kt
@@ -1,11 +1,14 @@
 package net.frozenblock.configurableeverything.util
 
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
 import net.fabricmc.loader.api.FabricLoader
 import net.frozenblock.lib.config.api.instance.json.JsonType
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.nio.file.Path
 import kotlin.io.path.Path
+import kotlin.io.path.pathString
 
 const val MOD_ID = "configurable_everything"
 const val MOD_NAME = "Configurable Everything"
@@ -38,6 +41,10 @@ val DATAPACKS_PATH: Path = Path("./config/$MOD_ID/datapacks")
 
 @JvmField
 val KOTLIN_SCRIPT_PATH: Path = Path("./config/$MOD_ID/scripts/")
+
+@JvmField
+@Environment(EnvType.CLIENT)
+val KOTLIN_CLIENT_SCRIPT_PATH: Path = Path("${KOTLIN_SCRIPT_PATH.pathString}/client/")
 
 // the idea is configurableeverything.kts but shorter
 const val KOTLIN_SCRIPT_EXTENSION: String = "cevt.kts"
