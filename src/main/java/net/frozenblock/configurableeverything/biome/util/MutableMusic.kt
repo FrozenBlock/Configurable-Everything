64b0e6bad2da73cda4c1545775a33a979f79acb3
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-11-23 21:09:53 -0600

    actually fix NullPointerException

--- src/main/java/net/frozenblock/configurableeverything/biome/util/MutableMusic.kt
+++ src/main/java/net/frozenblock/configurableeverything/biome/util/MutableMusic.kt
@@ -3,8 +3,14 @@ package net.frozenblock.configurableeverything.biome.util
 import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.core.Holder
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceKey
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.sounds.Music
 import net.minecraft.sounds.SoundEvent
+import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 data class MutableMusic(
     @JvmField var event: Holder<SoundEvent>?,
@@ -16,11 +22,13 @@ data class MutableMusic(
         @JvmField
         val CODEC: Codec<MutableMusic> = RecordCodecBuilder.create { instance ->
             instance.group(
-                SoundEvent.CODEC.fieldOf("sound").forGetter(MutableMusic::event),
+                SoundEvent.CODEC.optionalFieldOf("sound").forGetter { Optional.ofNullable(it.event) },
                 Codec.INT.fieldOf("min_delay").forGetter(MutableMusic::minDelay),
                 Codec.INT.fieldOf("max_delay").forGetter(MutableMusic::maxDelay),
                 Codec.BOOL.fieldOf("replace_current_music").forGetter(MutableMusic::replaceCurrentMusic)
-            ).apply(instance, ::MutableMusic)
+            ).apply(instance) { event, min, max, replace ->
+                MutableMusic(event.getOrNull(), min, max, replace)
+            }
         }
     }
 }
