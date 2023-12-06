f89facd51c8a397e218af089600a1bec0cc7a85d
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-12-05 19:26:51 -0600

    disable 1.1

--- src/main/java/net/frozenblock/configurableeverything/util/ConfigurableEverythingSharedConstants.kt
+++ src/main/java/net/frozenblock/configurableeverything/util/ConfigurableEverythingSharedConstants.kt
@@ -15,7 +15,7 @@ internal const val MOD_NAME = "Configurable Everything"
 /**
  * Enables update 1.1
  */
-internal const val ENABLE_EXPERIMENTAL_FEATURES = true
+internal const val ENABLE_EXPERIMENTAL_FEATURES = false
 
 internal val DEV_ENV = FabricLoader.getInstance().isDevelopmentEnvironment
 
