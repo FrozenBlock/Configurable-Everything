3094c5653ee56a3cbfcd3ce93624126cb3dda879
Treetrain1 <treetrain1@gmail.com>
2024-02-20 16:48:40 -0600

    fix space

--- src/main/java/net/frozenblock/configurableeverything/scripting/util/remap/Remapping.kt
+++ src/main/java/net/frozenblock/configurableeverything/scripting/util/remap/Remapping.kt
@@ -216,13 +216,11 @@ object Remapping {
     private val LICENSE_FILE: File = MAPPINGS_PATH.resolve("README.txt").toFile()
 
     private const val LICENSE: String =
-"""
-    # (c) 2020 Microsoft Corporation. These mappings are provided "as-is" and you bear the risk of using them.
-    You may copy and use the mappings for development purposes, but you may not redistribute the mappings complete and unmodified.
-    Microsoft makes no warranties, express or implied, with respect to the mappings provided here.
-    Use and modification of this document or the source code (in any form) of Minecraft: Java Edition is governed by the
-    Minecraft End User License Agreement available at https://account.mojang.com/documents/minecraft_eula.
-"""
+"""# (c) 2020 Microsoft Corporation. These mappings are provided "as-is" and you bear the risk of using them.
+You may copy and use the mappings for development purposes, but you may not redistribute the mappings complete and unmodified.
+Microsoft makes no warranties, express or implied, with respect to the mappings provided here.
+Use and modification of this document or the source code (in any form) of Minecraft: Java Edition is governed by the
+Minecraft End User License Agreement available at https://account.mojang.com/documents/minecraft_eula."""
 
     private fun saveLicense() {
         if (LICENSE_FILE.exists()) return
