53d8d9cd4a7f251f8a418bcbf6a221408f57f1cd
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2024-04-12 11:07:08 -0500

    Optimize Sodium config

--- src/main/java/net/frozenblock/configurableeverything/config/thirdparty/SodiumConfig.kt
+++ src/main/java/net/frozenblock/configurableeverything/config/thirdparty/SodiumConfig.kt
@@ -10,7 +10,7 @@ import net.frozenblock.lib.shadow.blue.endless.jankson.Comment
 data class SodiumConfig(
     @JvmField
     @Comment("Whether or not to disable Sodium's resource pack compatibility scanner.")
-    var disableResourcePackScanner: Boolean? = false,
+    var disableResourcePackScanner: Boolean = false,
 ) {
 
     companion object : JsonConfig<SodiumConfig>(
