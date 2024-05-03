82eb0005e768a1d9c9991041d5c4a35610de0ce0
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2024-05-03 11:00:49 -0500

    fix

--- src/main/java/net/frozenblock/configurableeverything/config/thirdparty/SodiumConfig.kt
+++ src/main/java/net/frozenblock/configurableeverything/config/thirdparty/SodiumConfig.kt
@@ -1,7 +1,7 @@
 package net.frozenblock.configurableeverything.config.thirdparty
 
 import net.frozenblock.configurableeverything.util.*
-import net.frozenblock.lib.config.api.instance.json.XjsConfig
+import net.frozenblock.lib.config.api.instance.xjs.XjsConfig
 import net.frozenblock.lib.config.api.registry.ConfigRegistry
 import net.frozenblock.lib.config.api.sync.annotation.UnsyncableConfig
 import net.frozenblock.lib.shadow.blue.endless.jankson.Comment
