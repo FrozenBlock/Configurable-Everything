30f0a63fb5dd916e777c448e11ebbbd20a8e0b83
Treetrain1 <treetrain1@gmail.com>
2024-05-04 02:09:08 -0500

    changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -220,7 +220,7 @@ dependencies {
     modApi(kotlin("scripting-dependencies-maven"))
 
     api("net.fabricmc:mapping-io:0.6.1")
-    api("net.fabricmc:tiny-remapper:0.10.1")
+    api("net.fabricmc:tiny-remapper:0.10.2")
 
     // FrozenLib
     if (local_frozenlib)
