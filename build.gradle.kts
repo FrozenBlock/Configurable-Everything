9e01b5d979fb9da291389dd2094ff85a36eb3ca7
Treetrain1 <treetrain1@gmail.com>
2024-05-21 03:50:01 -0500

    Upgrade Kotlin libs to 2.0 (rc3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version("1.9.24")
+    kotlin("jvm") version("2.0.0-RC3")
     id("fabric-loom") version("+")
     id("dev.yumi.gradle.licenser") version("+")
     id("org.ajoberstar.grgit") version("+")
@@ -210,14 +210,14 @@ dependencies {
     api(files("libs/fabric-loom-1.6.local-kotlin-remapper.jar"))?.let { shadowInclude(it) }
 
     // get deps manually because FKE cant give them to compile classpath without an error
-    api("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.9.0")
-    modApi(kotlin("scripting-common"))
-    modApi(kotlin("scripting-jvm"))
-    modApi(kotlin("scripting-jsr223"))
-    modApi(kotlin("scripting-jvm-host"))
-    modApi(kotlin("scripting-compiler-embeddable"))
-    modApi(kotlin("scripting-dependencies"))
-    modApi(kotlin("scripting-dependencies-maven"))
+    api(kotlin("metadata-jvm"))
+    api(kotlin("scripting-common"))
+    api(kotlin("scripting-jvm"))
+    api(kotlin("scripting-jsr223"))
+    api(kotlin("scripting-jvm-host"))
+    api(kotlin("scripting-compiler-embeddable"))
+    api(kotlin("scripting-dependencies"))
+    api(kotlin("scripting-dependencies-maven"))
 
     api("net.fabricmc:mapping-io:0.6.1")
     api("net.fabricmc:tiny-remapper:0.10.3")
