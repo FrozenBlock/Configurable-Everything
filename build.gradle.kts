3295118cb7b79c2666fe814cc7b2151ceb0032fe
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2024-07-08 11:33:16 -0500

    Change version numbers
    (cherry picked from commit 96c5d49281f63f2725b4485d253f1621d5913f17)

--- build.gradle.kts
+++ build.gradle.kts
@@ -352,7 +352,7 @@ artifacts {
 }
 
 fun getVersion(): String {
-    var version = "$mod_version+$minecraft_version"
+    var version = "$mod_version-mc$minecraft_version"
 
     if (release != null && !release) {
         version += "-unstable"
