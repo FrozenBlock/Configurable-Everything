ff81031918ead65e84cf11c4b7e8282022d82db5
Ethan Stokes <erstokes10@gmail.com>
2026-02-02 07:07:29 -0600

    Port mod protocol config

--- src/main/java/net/frozenblock/configurableeverything/config/ModProtocolConfig.kt
+++ src/main/java/net/frozenblock/configurableeverything/config/ModProtocolConfig.kt
@@ -1,9 +1,12 @@
 package net.frozenblock.configurableeverything.config
 
 import com.mojang.serialization.JsonOps
+import net.frozenblock.configurableeverything.util.CEConfig
 import net.frozenblock.configurableeverything.util.CESimpleConfig
 import net.frozenblock.lib.config.api.registry.ConfigRegistry
 import net.frozenblock.lib.config.api.sync.SyncBehavior
+import net.frozenblock.lib.config.v2.entry.ConfigEntry
+import net.frozenblock.lib.config.v2.entry.EntryType
 import net.frozenblock.lib.shadow.blue.endless.jankson.Comment
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.ComponentSerialization
@@ -14,86 +17,72 @@ private fun toJson(component: Component): String {
 
 // source: https://github.com/QuiltMC/quilt-standard-libraries/blob/1.20.2/library/core/registry/src/main/java/org/quiltmc/qsl/registry/impl/RegistryConfig.java
 // UNSYNCABLE
-data class ModProtocolConfig(
+object ModProtocolConfig : CEConfig("mod_protocol") {
     @JvmField
-    // UNSYNCABLE
-    @Comment(
-"""
-Mod protocol is a feature allowing you to prevent clients with mismatched settings to join.
-Client with mismatched values won't be able to connect to servers having this enabled.
-It should be used only for non-vanilla compatible modpacks!
-Protocol version. Needs to be the same on client and server. If it has value of -1, it won't be required by servers.
-"""
+    var modProtocolVersion: ConfigEntry<Int> = this.unsyncableEntry("modProtocolVersion", EntryType.INT, -1,
+        """
+        Mod protocol is a feature allowing you to prevent clients with mismatched settings to join.
+        Client with mismatched values won't be able to connect to servers having this enabled.
+        It should be used only for non-vanilla compatible modpacks!
+        Protocol version. Needs to be the same on client and server. If it has value of -1, it won't be required by servers.
+        """.trimIndent()
     )
-    var modProtocolVersion: Int = -1,
 
     @JvmField
-    // UNSYNCABLE
-    @Comment("Protocol id. It should be different for every modpack, to prevent joining with mismatched mods.")
-    var modProtocolId: String = "my_configurable_everything_modpack",
+    var modProtocolId: ConfigEntry<String> = this.unsyncableEntry("modProtocolId", EntryType.STRING, "my_configurable_everything_modpack",
+        "Protocol id. It should be different for every modpack, to prevent joining with mismatched mods."
+    )
 
     @JvmField
-    // UNSYNCABLE
-    @Comment("A mod protocol name. Used for easier identification. Doesn't affect functionality")
-    var modProtocolName: String = "My Configurable Everything Modpack",
+    var modProtocolName: ConfigEntry<String> = this.unsyncableEntry("modProtocolName", EntryType.STRING, "My Configurable Everything Modpack",
+        "A mod protocol name. Used for easier identification. Doesn't affect functionality"
+    )
 
     @JvmField
-    // UNSYNCABLE
-    @Comment("Message displayed for players joining with clients incompatible with Registry Sync. Supports strings and Minecraft's JSON text format.")
-    var missingRegistrySyncMessage: String = toJson(
-        Component.translatableWithFallback(
-            "frozenlib.registry_sync.unsupported_client",
-"""
-Unsupported (vanilla?) client!
-This server requires modded client to join!
-"""
-        )
-    ),
+    var missingRegistrySyncMessage: ConfigEntry<String> = this.unsyncableEntry("missingRegistrySyncMessage", EntryType.STRING,
+        toJson(
+            Component.translatableWithFallback(
+                "frozenlib.registry_sync.unsupported_client",
+                """
+                Unsupported (vanilla?) client!
+                This server requires modded client to join!
+                """.trimIndent()
+            )
+        ),
+        "Message displayed for players joining with clients incompatible with Registry Sync. Supports strings and Minecraft's JSON text format."
+    )
 
     @JvmField
-    // UNSYNCABLE
-    @Comment("Top part of the message displayed for players joining with incompatible clients. Supports strings and Minecraft's JSON text format.")
-    var mismatchedEntriesTopMessage: String = toJson(
-        Component.translatableWithFallback(
-            "frozenlib.registry_sync.failed_sync",
-"""
-Failed to synchronize client with the server!
-This can happen when client's and server's mods don't match.
-"""
-        )
-    ),
+    var mismatchedEntriesTopMessage: ConfigEntry<String> = this.unsyncableEntry("mismatchedEntriesTopMessage", EntryType.STRING,
+        toJson(
+            Component.translatableWithFallback(
+                "frozenlib.registry_sync.failed_sync",
+                """
+                Failed to synchronize client with the server!
+                This can happen when client's and server's mods don't match.
+                """.trimIndent(),
+            )
+        ),
+        "Top part of the message displayed for players joining with incompatible clients. Supports strings and Minecraft's JSON text format."
+    )
 
     @JvmField
-    // UNSYNCABLE
-    @Comment("Bottom part of the message displayed for players joining with incompatible clients. Supports strings and Minecraft's JSON text format.")
-    var mismatchedEntriesBottomMessage: String = "",
+    var mismatchedEntriesBottomMessage: ConfigEntry<String> = this.unsyncableEntry("mismatchedEntriesBottomMessage", EntryType.STRING, "",
+        "Bottom part of the message displayed for players joining with incompatible clients. Supports strings and Minecraft's JSON text format."
+    )
 
     @JvmField
-    // UNSYNCABLE
-    @Comment("Shows some details about why client couldn't connect.")
-    var mismatchedEntriesShowDetails: Boolean = true,
+    var mismatchedEntriesShowDetails: ConfigEntry<Boolean> = this.unsyncableEntry("mismatchedEntriesShowDetails", EntryType.BOOL, true,
+        "Shows some details about why client couldn't connect."
+    )
 
     @JvmField
-    // UNSYNCABLE
-    @Comment("Disables the Mod Protocol sync on server list/initial query.")
-    var disableModProtocolPing: Boolean = false,
+    var disableModProtocolPing: ConfigEntry<Boolean> = this.unsyncableEntry("disableModProtocolPing", EntryType.BOOL, false,
+        "Disables the Mod Protocol sync on server list/initial query."
+    )
 
     @JvmField
-    // UNSYNCABLE
-    @Comment("Disables the Mod Protocol requirement. USE AT YOUR OWN RISK!")
-    var disableModProtocol: Boolean = false,
-) {
-
-    companion object : CESimpleConfig<ModProtocolConfig>(
-        ModProtocolConfig::class,
-        "mod_protocol",
-        false
-    ) {
-        init {
-            ConfigRegistry.register(this)
-        }
-
-        @JvmStatic
-        fun get(): ModProtocolConfig = this.config()
-    }
+    var disableModProtocol: ConfigEntry<Boolean> = this.unsyncableEntry("disableModProtocol", EntryType.BOOL, false,
+        "Disables the Mod Protocol requirement. USE AT YOUR OWN RISK!"
+    )
 }
--- src/main/java/net/frozenblock/configurableeverything/mod_protocol/util/ModProtocolConfigUtil.kt
+++ src/main/java/net/frozenblock/configurableeverything/mod_protocol/util/ModProtocolConfigUtil.kt
@@ -9,21 +9,30 @@ import org.quiltmc.qsl.frozenblock.core.registry.impl.sync.server.ServerRegistry
 object ModProtocolConfigUtil : ModProtocol.LoadModProtocol {
 
     override fun load() {
-        val config = ModProtocolConfig.get()
-        ModProtocol.disableQuery = config.disableModProtocolPing
+        var disableModProtocolPing = ModProtocolConfig.disableModProtocolPing.get()
+        val disableModProtocol = ModProtocolConfig.disableModProtocol.get()
+        val modProtocolId = ModProtocolConfig.modProtocolId.get()
+        val modProtocolName = ModProtocolConfig.modProtocolName.get()
+        val modProtocolVersion = ModProtocolConfig.modProtocolVersion.get()
+        val missingRegistrySyncMessage = ModProtocolConfig.missingRegistrySyncMessage.get()
+        val mismatchedEntriesTopMessage = ModProtocolConfig.mismatchedEntriesTopMessage.get()
+        val mismatchedEntriesBottomMessage = ModProtocolConfig.mismatchedEntriesBottomMessage.get()
+        val mismatchedEntriesShowDetails = ModProtocolConfig.mismatchedEntriesShowDetails.get()
 
-        if (config.modProtocolVersion >= 0) {
-            ModProtocol.prioritizedEntry = ModProtocolDef("global:${config.modProtocolId}", config.modProtocolName, IntList.of(config.modProtocolVersion), false)
+        ModProtocol.disableQuery = disableModProtocolPing
+
+        if (modProtocolVersion >= 0) {
+            ModProtocol.prioritizedEntry = ModProtocolDef("global:${modProtocolId}", modProtocolName, IntList.of(modProtocolVersion), false)
             ModProtocol.prioritizedId = ModProtocol.prioritizedEntry.id
             ModProtocol.add(ModProtocol.prioritizedEntry)
         }
 
-        ServerRegistrySync.noRegistrySyncMessage = ServerRegistrySync.text(config.missingRegistrySyncMessage)
-        ServerRegistrySync.errorStyleHeader = ServerRegistrySync.text(config.mismatchedEntriesTopMessage)
-        ServerRegistrySync.errorStyleFooter = ServerRegistrySync.text(config.mismatchedEntriesBottomMessage)
+        ServerRegistrySync.noRegistrySyncMessage = ServerRegistrySync.text(missingRegistrySyncMessage)
+        ServerRegistrySync.errorStyleHeader = ServerRegistrySync.text(mismatchedEntriesTopMessage)
+        ServerRegistrySync.errorStyleFooter = ServerRegistrySync.text(mismatchedEntriesBottomMessage)
 
-        ServerRegistrySync.forceDisable = config.disableModProtocol
-        ServerRegistrySync.showErrorDetails = config.mismatchedEntriesShowDetails
+        ServerRegistrySync.forceDisable = disableModProtocol
+        ServerRegistrySync.showErrorDetails = mismatchedEntriesShowDetails
 
         if (ServerRegistrySync.forceDisable) {
             ServerRegistrySync.SERVER_SUPPORTED_PROTOCOL.clear()
